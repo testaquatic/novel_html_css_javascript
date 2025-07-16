cc59016a98b9cf4c36f5cdf09e361cced0809f8f
TestAquatic <86832451+testaquatic@users.noreply.github.com>
2025-07-16 20:42:57 +0900

    3장 종료

---
+++ ch03/textarea/script.js
@@ -0,0 +1,18 @@
+"use strict";
+const textarea = document.getElementById("inquiry");
+const counter = document.getElementById("counter");
+textarea?.addEventListener("input", function () {
+    if (!(this instanceof HTMLTextAreaElement && counter instanceof HTMLDivElement)) {
+        return;
+    }
+    const currentLength = this.value.length;
+    const maxLengthSt = this.getAttribute("maxLength");
+    const maxLength = maxLengthSt ? parseInt(maxLengthSt) : 0;
+    counter.textContent = `${currentLength}/${maxLength}`;
+    if (currentLength > maxLength * 0.8) {
+        counter.style.color = "red";
+    }
+    else {
+        counter.style.color = "blue";
+    }
+});
